bb7088b021107c987872c0e39b4064c6e8de3915
John Curran <john@talentwall.io>
2022-09-02 15:23:27 -0500

    http verbs

--- lib/shortcut.ex
+++ lib/shortcut.ex
@@ -3,16 +3,21 @@ defmodule Shortcut do
   Documentation for `Shortcut`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Shortcut.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp post(endpoint, body) do
+    HTTPoison.post("#{@api_url}/#{endpoint}", Jason.encode!(body), @headers)
+    |> normalize_response()
   end
+
+  defp get(endpoint) do
+    HTTPoison.get("#{@api_url}/#{endpoint}", @headers)
+    |> normalize_response()
+  end
+
+  defp put(endpoint, body) do
+    HTTPoison.put("#{@api_url}/#{endpoint}", Jason.encode!(body), @headers)
+    |> normalize_response()
+  end
+
+  defp normalize_response({:ok, %{body: body}}), do: body
+  defp normalize_response({:error, error}), do: error
 end
